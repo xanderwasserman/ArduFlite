02a21a4f03a73c76403ddce911e7999c80b9f2d4
Alexander Wasserman <alexander@fastcomm.com>
2025-03-17 17:25:21 +0100

    Added mqtt telemetry

--- ArduFlite.ino
+++ ArduFlite.ino
@@ -1,23 +1,41 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiManager.h>
+#include <PubSubClient.h>
 #include "HoldButton.h"
 #include "HoldButtonManager.h"
 #include "ArduFliteIMU.h"
-#include "ArduFliteController.h"   // quaternion-based controller
-#include "ServoManager.h"          // servo driver
-#include "FliteQuaternion.h"
+#include "ArduFliteController.h"
+#include "ServoManager.h"
+#include "ArduFliteMqttTelemetry.h"
+#include "TelemetryData.h"
+
+#define MQTT_SERVER "192.168.100.14" // Your MQTT broker IP address
+#define MQTT_PORT 1883
+#define MQTT_TOPIC "arduflite/telemetry"
 
 #define PRINT_EVERY_N_UPDATES 50
-
-ArduFliteIMU myIMU;
-ArduFliteController myController;
 
 #define LEFT_AIL_PIN    17
 #define RIGHT_AIL_PIN   16
 #define PITCH_PIN       4
 #define YAW_PIN         12
 
-ServoManager servoMgr(LEFT_AIL_PIN, RIGHT_AIL_PIN, PITCH_PIN, YAW_PIN);
+// Calibration Button definition
+#define CALIB_BUTTON_PIN 27
+#define CALIB_HOLD_TIME 3000
 
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+// 10 Hz telemetry frequency
+ArduFliteMqttTelemetry telemetry(mqttClient, 10.0f); 
+extern SemaphoreHandle_t telemetryMutex;
+extern TelemetryData telemetryData;
+
+ArduFliteIMU myIMU;
+ArduFliteController myController;
+
+ServoManager servoMgr(LEFT_AIL_PIN, RIGHT_AIL_PIN, PITCH_PIN, YAW_PIN);
 
 // Callback for calibrate button
 void onCalibrateHold() {
@@ -25,11 +43,21 @@ void onCalibrateHold() {
     myIMU.selfCalibrate();
 }
 
-// Calibration Button definition
-#define CALIB_BUTTON_PIN 27
-#define CALIB_HOLD_TIME 3000
-
 HoldButton calibrateButton(CALIB_BUTTON_PIN, CALIB_HOLD_TIME, onCalibrateHold, true, false, 50);
+
+void reconnect() {
+  while (!mqttClient.connected()) {
+    Serial.print("Connecting to MQTT...");
+    if (mqttClient.connect("ArduFliteESP32")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("Failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
 
 unsigned long lastMicros = 0;
 int print_counter = 0;
@@ -42,6 +70,17 @@ void setup() {
   // Note: INPUT_PULLUP means the pin is HIGH when not pressed,
   // and LOW when pressed (assuming the other side of the button goes to GND).
 
+  WiFiManager wifiManager;
+  if (!wifiManager.autoConnect("ArduFliteAP")) {
+    Serial.println("Failed to connect WiFi and hit timeout");
+    ESP.restart();
+  }
+
+  mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
+
+  telemetryMutex = xSemaphoreCreateMutex();
+  telemetry.begin();
+
   // Initialize IMU
   if (!myIMU.begin()) {
     Serial.println("IMU failed to init!");
@@ -52,12 +91,17 @@ void setup() {
 
   myController.setDesiredOrientation(firstQ);
 
+  Serial.printf("ArduFlite Controller initialised\n");
+
   calibrateButton.begin();
   HoldButtonManager::registerButton(calibrateButton);
   Serial.println("Press and hold the button for 3s at any time to calibrate the IMU.");
 }
 
-void loop() {
+void loop() 
+{
+  if (!mqttClient.connected()) reconnect();
+  mqttClient.loop();
 
   // Calculate delta time
   unsigned long currentMicros = micros();
@@ -85,7 +129,34 @@ void loop() {
   // send commands to servos
   servoMgr.writeCommands(rollCmd, pitchCmd, yawCmd);
 
-  if (print_counter++ >= PRINT_EVERY_N_UPDATES) {
+  // Update telemetry data
+  if (xSemaphoreTake(telemetryMutex, 5) == pdTRUE) {
+      telemetryData.accelX = myIMU.getAccelX();
+      telemetryData.accelY = myIMU.getAccelY();
+      telemetryData.accelZ = myIMU.getAccelZ();
+
+      telemetryData.gyroX = myIMU.getGyroX();
+      telemetryData.gyroY = myIMU.getGyroY();
+      telemetryData.gyroZ = myIMU.getGyroZ();
+
+      telemetryData.qw = myIMU.getQw();
+      telemetryData.qx = myIMU.getQx();
+      telemetryData.qy = myIMU.getQy();
+      telemetryData.qz = myIMU.getQz();
+
+      telemetryData.pitch = myIMU.getPitch();
+      telemetryData.roll = myIMU.getRoll();
+      telemetryData.yaw = myIMU.getYaw();
+
+      telemetryData.rollCmd = rollCmd;
+      telemetryData.pitchCmd = pitchCmd;
+      telemetryData.yawCmd = yawCmd;
+
+      xSemaphoreGive(telemetryMutex);
+  }
+
+  if (print_counter++ >= PRINT_EVERY_N_UPDATES) 
+  {
     
     Serial.print("Acceleration: ");
     Serial.print(myIMU.getAccelX());
@@ -94,12 +165,12 @@ void loop() {
     Serial.print(", ");
     Serial.println(myIMU.getAccelZ());
 
-    // Serial.print("Gyroscope: ");
-    // Serial.print(myIMU.getGyroX());
-    // Serial.print(",");
-    // Serial.print(myIMU.getGyroY());
-    // Serial.print(",");
-    // Serial.println(myIMU.getGyroZ())
+    Serial.print("Gyroscope: ");
+    Serial.print(myIMU.getGyroX());
+    Serial.print(",");
+    Serial.print(myIMU.getGyroY());
+    Serial.print(",");
+    Serial.println(myIMU.getGyroZ());
     
     // Serial.print("Quarternion: ");
     // Serial.print(myIMU.getQw());
@@ -124,5 +195,8 @@ void loop() {
     print_counter = 0;
   }
   
+  //print quaternion for visualiser
+  // Serial.printf("%f,%f,%f,%f\n", myIMU.getQw(), myIMU.getQx(), myIMU.getQy(), myIMU.getQz());
+
   delay(10); // ~100 Hz print
 }
---
+++ TelemetryData.h
@@ -0,0 +1,17 @@
+#ifndef TELEMETRY_DATA_H
+#define TELEMETRY_DATA_H
+
+#include <Arduino.h>
+
+struct TelemetryData {
+    float accelX, accelY, accelZ;
+    float gyroX, gyroY, gyroZ;
+    float qw, qx, qy, qz;
+    float pitch, roll, yaw;
+    float rollCmd, pitchCmd, yawCmd;
+};
+
+extern SemaphoreHandle_t telemetryMutex;
+extern TelemetryData sharedTelemetryData;
+
+#endif

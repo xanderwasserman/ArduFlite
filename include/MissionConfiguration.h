8bf0f3bf4ac7c659089d73ba19ed19e8f6c25be4
Alexander Wasserman <alexander@fastcomm.com>
2025-05-11 13:06:45 +0200

    Adjust mission timing

--- include/MissionConfiguration.h
+++ include/MissionConfiguration.h
@@ -14,9 +14,9 @@
 namespace MissionConfig 
 {
     constexpr MissionPlanner::Step MISSION_STEPS[] = {
-        {  0.0f,  2.0f,  0.0f, 1000 },  // climb 2° for 1 s
+        {  0.0f,  2.0f,  0.0f, 2000 },  // climb 2° for 2 s
         { 10.0f,  0.0f,  0.0f, 1000 },  // roll +10° for 1 s
-        {  0.0f,  0.0f,  0.0f,  500 },  // level for 0.5 s
+        {  0.0f,  0.0f,  0.0f, 1000 },  // level for 0.5 s
         {-10.0f,  0.0f,  0.0f, 1000 },  // roll –10° for 1 s
         {  0.0f,  0.0f,  0.0f, 1000 },  // level for 1 s
     };

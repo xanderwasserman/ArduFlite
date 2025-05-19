174b69bc85a1b33585d0704f758fa27359d20766
Alexander Wasserman <alexander@fastcomm.com>
2025-05-19 21:47:28 +0200

    Update controllers after flight test

--- include/ControllerConfiguration.h
+++ include/ControllerConfiguration.h
@@ -13,9 +13,9 @@
 
 namespace AttitudeControllerConfig 
 {
-    constexpr PIDConfig DEFAULT_ROLL_PID  = {200.0f,  0.0f,   0.0f, 60.0f, 100.0f, 0.1f};
-    constexpr PIDConfig DEFAULT_PITCH_PID = {200.0f,  0.0f,   0.0f, 60.0f, 100.0f, 0.1f};
-    constexpr PIDConfig DEFAULT_YAW_PID   = {200.0f,  0.0f,   0.0f, 60.0f, 100.0f, 0.1f};
+    constexpr PIDConfig DEFAULT_ROLL_PID  = {200.0f,  20.0f,   30.0f, 100.0f, 100.0f, 0.1f};
+    constexpr PIDConfig DEFAULT_PITCH_PID = {200.0f,  20.0f,   30.0f, 100.0f, 100.0f, 0.1f};
+    constexpr PIDConfig DEFAULT_YAW_PID   = {200.0f,  20.0f,   30.0f, 100.0f, 100.0f, 0.1f};
 
     constexpr float ATTITUDE_DEADBAND_RADS = 0.002f;
 
@@ -23,9 +23,9 @@ namespace AttitudeControllerConfig
 
 namespace RateControllerConfig 
 {
-    constexpr PIDConfig DEFAULT_ROLL_PID  = {0.015f,    0.0032f,  0.00125f,     1.0f, 0.6f, 0.03f};
-    constexpr PIDConfig DEFAULT_PITCH_PID = {0.009f,    0.0025f,  0.0036f,      1.0f, 0.6f, 0.02f};
-    constexpr PIDConfig DEFAULT_YAW_PID   = {0.015f,    0.0f,     0.00125f,    1.0f, 0.6f, 0.03f};
+    constexpr PIDConfig DEFAULT_ROLL_PID  = {0.035f,    0.005f,  0.003f,     1.0f, 1.0f, 0.03f};
+    constexpr PIDConfig DEFAULT_PITCH_PID = {0.01f,    0.0025f,  0.005f,      1.0f, 1.0f, 0.02f};
+    constexpr PIDConfig DEFAULT_YAW_PID   = {0.01f,    0.0f,     0.003f,    1.0f, 1.0f, 0.03f};
 
     constexpr float outLpAlpha = 0.1f;
 

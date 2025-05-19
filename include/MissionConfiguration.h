745372f25e3e823a3edadba72ed949087090f898
Alexander Wasserman <alexander@fastcomm.com>
2025-05-19 21:47:09 +0200

    Update mission to roll more

--- include/MissionConfiguration.h
+++ include/MissionConfiguration.h
@@ -15,9 +15,9 @@ namespace MissionConfig
 {
     constexpr MissionPlanner::Step MISSION_STEPS[] = {
         {  0.0f,  5.0f,  0.0f, 2000 },  // climb 5° for 2 s
-        { 10.0f,  0.0f,  0.0f, 1000 },  // roll +10° for 1 s
+        { 20.0f,  0.0f,  0.0f, 1000 },  // roll +10° for 1 s
         {  0.0f,  0.0f,  0.0f, 1000 },  // level for 0.5 s
-        {-10.0f,  0.0f,  0.0f, 1000 },  // roll –10° for 1 s
+        {-20.0f,  0.0f,  0.0f, 1000 },  // roll –10° for 1 s
         {  0.0f,  0.0f,  0.0f, 1000 },  // level for 1 s
     };
 } // namespace MissionConfig

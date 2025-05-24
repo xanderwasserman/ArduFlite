d19508e83d67732d5a1000b2766bae86536867bd
Alexander Wasserman <xander@MacBook-Pro.localdomain>
2025-05-24 18:29:15 +0200

    Update formatting

--- include/ControllerConfiguration.h
+++ include/ControllerConfiguration.h
@@ -13,21 +13,21 @@
 
 namespace AttitudeControllerConfig 
 {
-    constexpr PIDConfig DEFAULT_ROLL_PID  = {200.0f,  15.0f,   30.0f, 100.0f, 100.0f, 0.1f};
-    constexpr PIDConfig DEFAULT_PITCH_PID = {160.0f,  15.0f,   30.0f, 100.0f, 100.0f, 0.1f};
-    constexpr PIDConfig DEFAULT_YAW_PID   = {200.0f,  15.0f,   30.0f, 100.0f, 100.0f, 0.1f};
+    constexpr PIDConfig DEFAULT_ROLL_PID    = {200.0f,    15.0f,      30.0f,      100.0f,     100.0f,     0.1f};
+    constexpr PIDConfig DEFAULT_PITCH_PID   = {160.0f,    15.0f,      30.0f,      100.0f,     100.0f,     0.1f};
+    constexpr PIDConfig DEFAULT_YAW_PID     = {200.0f,    15.0f,      30.0f,      100.0f,     100.0f,     0.1f};
 
-    constexpr float ATTITUDE_DEADBAND_RADS = 0.001f;
+    constexpr float ATTITUDE_DEADBAND_RADS  = 0.001f;
 
 } // namespace AttitudeControllerConfig
 
 namespace RateControllerConfig 
 {
-    constexpr PIDConfig DEFAULT_ROLL_PID  = {0.03f,    0.01f,       0.003f,     1.0f,   1.0f,   0.1f};
-    constexpr PIDConfig DEFAULT_PITCH_PID = {0.01f,    0.003f,      0.005f,     0.5f,   1.0f,   0.1f};
-    constexpr PIDConfig DEFAULT_YAW_PID   = {0.01f,    0.002f,      0.003f,     0.5f,   1.0f,   0.1f};
+    constexpr PIDConfig DEFAULT_ROLL_PID    = {0.03f,     0.01f,       0.003f,     1.0f,      1.0f,       0.1f};
+    constexpr PIDConfig DEFAULT_PITCH_PID   = {0.01f,     0.003f,      0.005f,     0.5f,      1.0f,       0.1f};
+    constexpr PIDConfig DEFAULT_YAW_PID     = {0.01f,     0.002f,      0.003f,     0.5f,      1.0f,       0.1f};
 
-    constexpr float outLpAlpha = 0.01f;
+    constexpr float outLpAlpha              = 0.01f;
 
 } // namespace RateControllerConfig
 

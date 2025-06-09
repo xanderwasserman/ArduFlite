fa27443c6c2ec00aec8ab3a3f8cbe82339614117
Alexander Wasserman <alexander@fastcomm.com>
2025-06-09 17:33:02 +0200

    Completed CRSF RX callbacks

--- src/receiver/crsf/ArdufliteCRSFCallbacks.cpp
+++ src/receiver/crsf/ArdufliteCRSFCallbacks.cpp
@@ -11,27 +11,45 @@
 #include "src/utils/CommandSystem.h"
 #include "src/utils/Logging.h"
 
-void onRoll(uint8_t ch, float v) {
-
+void onRoll(uint8_t ch, float v) 
+{
+    SystemCommand cmd;
+    cmd.type = CMD_RECEIVER_SETPOINT_ROLL;
+    cmd.attitudeConfig.roll = v;
+    CommandSystem::instance().pushCommand(cmd);
 }
 
-void onPitch(uint8_t ch, float v) {
-
+void onPitch(uint8_t ch, float v) 
+{
+    SystemCommand cmd;
+    cmd.type = CMD_RECEIVER_SETPOINT_PITCH;
+    cmd.attitudeConfig.pitch = v;
+    CommandSystem::instance().pushCommand(cmd);
 }
-void onYaw(uint8_t ch, float v) {
 
+void onYaw(uint8_t ch, float v) 
+{
+    SystemCommand cmd;
+    cmd.type = CMD_RECEIVER_SETPOINT_YAW;
+    cmd.attitudeConfig.yaw = v;
+    CommandSystem::instance().pushCommand(cmd);
 }
-void onModeSwitch(uint8_t ch, float v) {
+
+void onModeSwitch(uint8_t ch, float v) 
+{
 
     SystemCommand cmd;
     cmd.type = CMD_SET_MODE;
     bool newState  = (v > 0.5f);  // v is 0.0 or 1.0, but guard anyway
 
-    if (!newState) { //Stabilized mode when receiving false
+    if (!newState) //rate mode when receiving false
+    { 
         LOG_INF("Changing Flight Control mode to: RATE_MODE.");
         cmd.mode = RATE_MODE;
         CommandSystem::instance().pushCommand(cmd);
-    } else {
+    } 
+    else 
+    {
         LOG_INF("Changing Flight Control mode to: ATTITUDE_MODE.");
         cmd.mode = ATTITUDE_MODE;
         CommandSystem::instance().pushCommand(cmd);
